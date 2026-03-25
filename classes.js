1b5614c15491002ac6d0f5961640dd679f0e0446
alessiaiul26 <alessia.iulianetti1999@gmail.com>
2026-03-25 16:29:10 +0100

    feat: completata classe Articolo e gestione scorte magazzino

--- classes.js
+++ classes.js
@@ -31,3 +31,47 @@ let progetto3 = new Progetto("Ristorante da Mario", "Sito Web", 1200)
 console.log(progetto1.riepilogoCompleto());
 console.log(progetto2.riepilogoCompleto());
 console.log(progetto3.riepilogoCompleto());
+
+class Utente {
+    constructor(username="Gigi12", email="gigi@email.it", ruolo="Grafico"){
+        this.username = username;
+        this.email = email;
+        this.ruolo = ruolo;
+    }
+    cambiaruolo(nuovoRuolo){
+        this.ruolo = nuovoRuolo
+        return `Ruolo di ${this.username} aggiornato a ${nuovoRuolo}`;
+    }
+    verificaEmail(){
+       if (this.email.includes("@")){
+        return "Email valida";
+       } else {
+        return "Email non valida"
+       }
+    }
+}
+let utenteDefault = new Utente();
+console.log(utenteDefault.cambiaruolo("fruttivendolo"));
+let utente1 = new Utente("Alessia", "alessia@libero.it", "Admin")
+console.log(utente1.cambiaruolo("Super-Admin"));
+console.log(utente1.verificaEmail());
+
+class Articolo {
+    constructor(nome="Iphone", prezzi=500, quantita=26){
+        this.nome= nome;
+        this.prezzi= prezzi;
+        this.quantita=quantita;
+    }
+    vendi(unita){
+        this.quantita = this.quantita - unita;
+        if(this.quantita <= 0){
+            console.log(`${this.nome} esaurito!`)
+        }
+        else{
+            console.log(`vendute ${unita} unita di ${this.nome}. Rimasti: ${this.quantita}`)
+        }
+    }
+}
+ let mioTelefono = new Articolo();
+ mioTelefono.vendi(5);
+ mioTelefono.vendi(30);
